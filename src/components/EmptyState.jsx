df5e028ecc4d97bac857786376b49aa50ad3a64f
Jennifer Jansson <jenniferjansson92@gmail.com>
2026-01-16 09:46:55 +0100

    created empty state and implemented in todolist

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,41 @@
+
+import styled from "styled-components";
+
+const EmptyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const EmptyTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: #333;
+`;
+
+const EmptyText = styled.p`
+  font-size: 16px;
+  color: #666;
+`;
+
+ const CheckIcon = styled.img`
+    width: 30px;
+    height: 30px;
+    margin-left: 10px;
+
+
+  `;
+
+export const EmptyState = () => {
+
+return (
+  <EmptyContainer>
+    <EmptyTitle>No todos here!
+    <CheckIcon src="/check.svg" alt="No todos" />
+    </EmptyTitle>
+    <EmptyText>Either you deserve a break or you it's time to add some todos!</EmptyText>
+  </EmptyContainer>
+)
+}
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,9 +1,14 @@
 import { useTodoStore } from '../store/todoStore.jsx'
 import { TodoItem } from './todoItem.jsx';
+import { EmptyState } from './emptyState.jsx';
 
 export const TodoList = () => {
   const todos = useTodoStore ((state) => state.todos)
 
+
+   if (todos.length === 0) {
+    return <EmptyState />
+  }
 
   return (
     <>
